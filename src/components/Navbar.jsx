bb8cd038e0ef6a334afecffee4e0eaee855382ad
arielvergarautn <arie@est.frba.utn.edu.ar>
2020-07-16 22:20:00 -0300

    encabezado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,85 @@
 import React from 'react'
+import '../public/css/Navbar.css'
+import {isMobile} from '../helpers/window'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
-const Navbar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
-            <div className="navbar-content">
-                <a className="navbar-brand" href="#">
-                    Plomería Bautista Ángel
+
+
+const Navbar = (props) => {
+
+    console.log(isMobile());
+
+    const desktopContent = () => {
+        return(
+            <div id="desktop-actions" className="btn-group dropleft">
+                 <a className="navbar-action" href="#about-us">
+                    Sobre nosotros
                 </a>
-                <button className="navbar-toggler" type="button" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                <a className="navbar-action" href="#services">
+                    Servicios
+                </a>
+                <a className="navbar-action" href="#prices">
+                    Cotizaciones
+                </a>
+                <a className="navbar-action" href="#contact">
+                    Contacto
+                </a>
+            </div>
+        )
+    }
+
+    const mobileContent = () =>{
+        return(
+            <div id="mobile-actions" className="btn-group dropleft">
+                <button className="btn-actions" data-toggle="dropdown">
+                    <FontAwesomeIcon icon={faBars} />
                 </button>
-                <div className="collapse navbar-collapse" id="navbarResponsive">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#home">
-                                Inicio
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#about-us">Sobre nosotros</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#services">Servicios</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#prices">Solicitar una cotización</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#contact">Contacto</a>
-                        </li>
-                    </ul>
+
+                <div className="dropdown-menu">
+                    <button
+                        className="dropdown-item"
+                        type="button"
+                    >
+                        <a href="#about-us" className="navbar-action">Sobre nosotros</a>
+                    </button>
+                    <button
+                        className="dropdown-item"
+                        type="button"
+                    >
+                        <a href="#services" className="navbar-action">Servicios</a>
+                    </button>
+                    <button
+                        className="dropdown-item"
+                        type="button"
+                    >
+                        <a href="#prices" className="navbar-action">Cotizaciones</a>
+                    </button>
+                    <button
+                        className="dropdown-item"
+                        type="button"
+                    >
+                        <a href="#contact" className="navbar-action">Contactenos</a>
+                    </button>
                 </div>
             </div>
-        </nav>
+        )
+    }
 
+
+    return (
+        <nav id="navbar" className="navbar navbar-light bg-light top-navbar px-4">
+            <a className="navbar-brand" href="/">
+                Plomería Bautista Ángel
+            </a>
+            {
+                isMobile() ? (
+                    mobileContent()
+                ) : (
+                    desktopContent()
+                )
+            }
+        </nav >
     )
 }
 
