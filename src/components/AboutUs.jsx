5e4648477fe87e6727bf8ed01e16fbebbc4c0892
arielvergarautn <arie@est.frba.utn.edu.ar>
2020-07-16 22:53:34 -0300

    about us

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import '../public/css/AboutUs.css'
+import diego from '../public/images/Diego.jpg' 
 
 const AboutUs = () => {
     return (
-        <div id="about-us"  style={{height:"500px"}}>
-            Sobre nosotros
+        <div id="about-us" className="text-center p-5">
+            <div className="title">
+                <h2>Sobre nosotros</h2>
+            </div>
+            <div className="details">
+                <label>Somos una empresa dedicada a distintos servicios domesticos.</label>
+            </div>
+            <div className="pictures">
+                <div className="picture pt-5 pb-3">
+                    <img src={diego} alt="" srcset=""/>
+                </div>
+                <h3>Diego Vergara</h3>
+            </div>
         </div>
     )
 }
